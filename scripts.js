c60d247e4b090d2b9bdf23b081e6f695cedec95a
Eric Barreto <ericbarreto521@gmail.com>
2023-11-29 15:15:58 -0300

    Menu hamburguer concluido

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
-const elementoNavegation = document.querySelectorAll("#navigation")
+const navBar = document.querySelector(".navigation")
 
+const menuButton = document.querySelector(".menu-button")
+
+const closeButton = document.querySelector(".close-button")
+
+menuButton.addEventListener("click", () => {
+    navBar.classList.toggle('show-menu')
+});
+
+closeButton.addEventListener("click", () => {
+    navBar.classList.toggle('show-menu')
+});
