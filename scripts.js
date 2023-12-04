a8d7b0e2e052b133289f048b46cb50fefbe9457c
Eric Barreto <ericbarreto521@gmail.com>
2023-12-04 09:43:39 -0300

    Criando modo claro escuro

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 const navBar = document.querySelector(".navigation")
-
 const menuButton = document.querySelector(".menu-button")
-
 const closeButton = document.querySelector(".close-button")
+const logo = document.querySelector(".logo-theme")
+const change_icon = document.querySelector(".change-theme")
+const body = document.body
+
+function toggleTheme(){
+    body.classList.toggle("dark")
+    if (body.classList.contains("dark")){
+        change_icon.setAttribute("src", "img/bx-sun.svg")
+        logo.setAttribute("src", "img/Logo-header-white.png")
+        menuButton.setAttribute("src", "img/menu-white.svg")
+        closeButton.setAttribute("src", "img/menu-close.svg")
+    } else {
+        change_icon.setAttribute("src", "img/bx-moon.svg.png")
+        logo.setAttribute("src", "img/Logo header.png")
+        menuButton.setAttribute("src", "img/menu-dark.svg")
+        closeButton.setAttribute("src", "img/menu-close-dark.svg")
+    }
+    
+}
 
 menuButton.addEventListener("click", () => {
     navBar.classList.toggle('show-menu')
@@ -11,3 +28,7 @@ menuButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     navBar.classList.toggle('show-menu')
 });
+
+change_icon.addEventListener("click", () =>{
+    toggleTheme()
+})
